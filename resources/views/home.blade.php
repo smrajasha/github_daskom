34add6ee091de9701236b236d4d1ce7f2dc2ec1b
Raja <rajagerrard57@gmail.com>
2021-05-21 23:31:26 +0700

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+
+    <body>
+        <p>Hello World</p>
+        <p>Halo Dunia</p>
+        <p>Hai Dunia</p>
+    </body>
+</html>
